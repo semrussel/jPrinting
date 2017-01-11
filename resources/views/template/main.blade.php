698748552630b04f7cee6d1d3eae8374af48d225
RJ45 <semrussel@gmail.com>
2017-01-11 21:09:42 +0800

    main.blade

--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -84,12 +84,6 @@
 
     <script href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
 
-    <!-- <script type="text/javascript">
-         if ({{ Input::old('autoOpenModal', 'false') }}) {
-            //JavaScript code that open up your modal.
-            $('#login-modal').modal('show');
-        }
-    </script> -->
 
 </body>
 
